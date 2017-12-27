c6cdd0ba5be235303d73ecc1190bbf684188f39f
Nguyen Van Thieu <thieumao@gmail.com>
2017-12-28 02:49:53 +0700

    Core ML Sample

--- CoreMLTutorial/ViewController.swift
+++ CoreMLTutorial/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import CoreML
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     
     @IBOutlet weak var resultLabel: UILabel!
+    
+    var model: Inceptionv3!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        model = Inceptionv3()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,14 +56,58 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
         dismiss(animated: true, completion: nil)
     }
     
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+//        // 4
+//        picker.dismiss(animated: true)
+//        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
+//            return
+//        }
+//
+//        imageView.image = image
+//        resultLabel.text = "Analyzing Image..."
+//    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        // 4
         picker.dismiss(animated: true)
         guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
             return
         }
         
-        imageView.image = image
         resultLabel.text = "Analyzing Image..."
+        // 1
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        // 2
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) //3
+        
+        context?.translateBy(x: 0, y: newImage.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        // 3
+        UIGraphicsPushContext(context!)
+        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        imageView.image = newImage
+        
+        // continue
+        guard let prediction = try? model.prediction(image: pixelBuffer!) else {
+            return
+        }
+        
+        resultLabel.text = "\(prediction.classLabel)."
     }
+    
 }
